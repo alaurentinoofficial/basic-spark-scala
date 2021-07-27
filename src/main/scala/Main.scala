ba4b890d7a3dd5359d6ccc1965dc6afccb8dcde0
Anderson Laurentino <anderson@laurentino.me>
2021-07-27 09:53:48 -0300

    Modify the spark log level to error && Create a sequence to simplify the data creation && Use transformation with the helper functions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,42 +1,39 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.{Row}
+import org.apache.spark.sql.{Row, DataFrame}
 import org.apache.spark.sql.types._
 
-object Main extends App {
-    val spark = SparkSession
-        .builder()
-        .appName("Spark SQL basic example")
-        .master("local")
-        .config("spark.executor.instances", "4")
-        .config("spark.executor.memory", "2g")
-        .getOrCreate()
+object Main {
+    def main(args: Array[String]) {
+        val spark = SparkSession
+            .builder()
+            .appName("Spark SQL basic example")
+            .master("local")
+            .getOrCreate()
+
+        spark.sparkContext.setLogLevel("ERROR")
+        
+        import spark.implicits._
+        
+        // Create a sequence and convert to Dataframe using spark implicits
+        val vote_df = Seq(
+            ("A", 20, true),
+            ("B", 34, false),
+            ("C", 17, true),
+            ("D", 67, false)
+        ).toDF("Name", "Age", "Voted")
     
-    import spark.implicits._
+        // Show the dataframe
+        vote_df.show()
     
-    val voteRDD = spark.sparkContext.parallelize(List(
-        Row("Anderson", 20, true),
-        Row("Fulano", 34, false),
-        Row("Ciclano", 17, true),
-        Row("Deltrano", 67, false)
-    ))
-
-    val voteSchema = StructType(List(
-        StructField("Name", StringType, true),
-        StructField("Age", IntegerType, true),
-        StructField("Voted", BooleanType, true)
-    ))
-
-    val vote_df = spark.createDataFrame(voteRDD, voteSchema)
-
-    vote_df.show()
-
-    vote_df
-        .filter($"Voted" && $"Name" =!= "Anderson")
-        .show()
-
-    vote_df
-        .groupBy($"Voted")
-        .agg(sum($"Age").alias("AgeAvg"))
-        .explain()
+        // Explain the average transformation
+        vote_df
+            .transform(Helper.groupKeyAvgNumber("Voted", "Age"))
+            .explain()
+        
+        // Show the average transformation
+        vote_df
+            .transform(Helper.groupKeyAvgNumber("Voted", "Age"))
+            .show()
+    }
 }
